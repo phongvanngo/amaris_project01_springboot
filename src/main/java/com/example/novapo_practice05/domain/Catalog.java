00dfefa43e287455e5d3f2be3fc8c21aaea543bb
vanphong.ngo_amaris <19520834@gm.uit.edu.vn>
2022-10-26 00:41:25 -0700

    update authentication

--- src/main/java/com/example/novapo_practice05/domain/Catalog.java
+++ src/main/java/com/example/novapo_practice05/domain/Catalog.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @Table(name="catalog")
 public class Catalog extends AbstractAuditingEntity{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private long id;
 
     @NotNull
